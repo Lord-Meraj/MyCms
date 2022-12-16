b7fdfe9772e6e83c32349addbbf140087f8601af
Lord_Meraj <merajp8@gmail.com>
2022-12-16 22:22:48 +0330

    change Icon and add pakage

--- Index.php
+++ Index.php
@@ -1,79 +1,81 @@
 <html lang="en">
-    <head>
-        <title style="color: brown">Sidebar 6</title>
-        <meta name="author" content="Zaur">
-        <meta descryption content="Presentation of website">
-        <meta name="keywords" content="technology, cyber security, software">
-        <meta http-equiv="refresh" content="100">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="sidebar6.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-		<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css'  rel='stylesheet'>
-        <link rel="stylesheet" href="src/css/style.css">
-    </head>
-    <body>
-		<div class="sidebar">
-            <header class="sidebar_header">
-                <p>Instagram</p>
-				<i class="fa-brands fa-instagram"></i>
-			</header>
-			<nav class="sidebar_menu">
-                <button>
-                    <span>
-                        <i class="fa-solid fa-house"></i>
-						<span>Home</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <i class="fa-solid fa-magnifying-glass"></i>
-						<span>Search</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <i class="fa-solid fa-compass"></i>
-						<span>Explore</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <i class="fa-solid fa-location-arrow">
-                            <span>13</span>
-						</i>
-						<span>Messages</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <i class="fa-regular fa-heart">
-                            <em></em>
-						</i>
-						<span>Notifications</span>
-					</span>
-				</button>
-				<button>
-					<span>
-						<i class="fa-solid fa-circle-plus"></i>
-						<span>Create</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzAqKERFeE4h-K3vnOWQSWJmxRTKTApVjEg&usqp=CAU">
-						<span>Profile</span>
-					</span>
-				</button>
-				<button>
-                    <span>
-                        <i class="fa-solid fa-bars"></i>
-						<span>More</span>
-					</span>
-				</button>
-			</nav>
-		</div>
-        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    </body>
-    </html>
-    
+
+<head>
+    <title style="color: brown">Sidebar 6</title>
+    <meta name="author" content="Zaur">
+    <meta descryption content="Presentation of website">
+    <meta name="keywords" content="technology, cyber security, software">
+    <meta http-equiv="refresh" content="100">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="src/lib/font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="src/lib/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="src/css/style.css">
+</head>
+
+<body>
+    <div class="sidebar">
+        <header class="sidebar_header">
+            <p>CMS</p>
+            <i class="fa fa-instagram"></i>
+        </header>
+        <nav class="sidebar_menu">
+            <button>
+                <span>
+                    <i class="fa fa-home"></i>
+                    <span>Home</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-search"></i>
+                    <span>Search</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-compass"></i>
+                    <span>Explore</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-location-arrow">
+                        <span>13</span>
+                    </i>
+                    <span>Messages</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-heart">
+                        <em></em>
+                    </i>
+                    <span>Notifications</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-plus"></i>
+                    <span>Create</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzAqKERFeE4h-K3vnOWQSWJmxRTKTApVjEg&usqp=CAU">
+                    <span>Profile</span>
+                </span>
+            </button>
+            <button>
+                <span>
+                    <i class="fa fa-bars"></i>
+                    <span>More</span>
+                </span>
+            </button>
+        </nav>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+</body>
+
+</html>
